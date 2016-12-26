463f252c77855a57e5e367639666e85b60769d33
Jonas Bandi <dev@jonasbandi.net>
2016-12-26 20:24:58 +0100

    Checkout component

---
+++ src/app/checkout/checkout.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-checkout',
+  templateUrl: './checkout.component.html',
+})
+export class CheckoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
