19943cce30ab6526881a2fc75c9824939434c64c
Jonas Bandi <dev@jonasbandi.net>
2016-12-31 14:26:00 +0100

    Basic registration is working

--- src/app/core/bookstore-core.module.ts
+++ src/app/core/bookstore-core.module.ts
@@ -6,9 +6,10 @@ import {AuthService} from "./service/auth.service";
 import {LoginComponent} from "./components/login.component";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, HttpModule],
+  imports: [CommonModule, RouterModule, FormsModule, HttpModule],
   declarations: [LoginComponent],
   exports: [LoginComponent],
   providers: [AuthService, LoggedInGuard, BookstoreHttpClientService],
