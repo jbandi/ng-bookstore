819ae631848a6cace3de03daf108cd892c5b48d1
Jonas Bandi <dev@jonasbandi.net>
2016-12-31 11:18:46 +0100

    Cleanup

--- src/app/account/account.routing.ts
+++ src/app/account/account.routing.ts
@@ -2,11 +2,11 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AccountComponent} from "./components/account.component";
 
-const shoppingcartRoutes: Routes = [
+const shoppingCartRoutes: Routes = [
     {
         path: '',
         component: AccountComponent
     },
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forChild(shoppingcartRoutes);
+export const routing: ModuleWithProviders = RouterModule.forChild(shoppingCartRoutes);
