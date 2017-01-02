f66d46e85ffd0629a6e00109762b480fb9418713
Jonas Bandi <dev@jonasbandi.net>
2017-01-02 23:36:16 +0100

    Beautifying registration from

--- src/app/registration/components/registration.component.ts
+++ src/app/registration/components/registration.component.ts
@@ -7,41 +7,75 @@ import {RegistrationService} from "../service/registration.service";
 <h2>Registration</h2>
 <form novalidate  [formGroup]="customerForm" (ngSubmit)="onSubmit()">
 
-<div formGroupName="customer">
+  <div formGroupName="customer">
+  
+    <div class="form-group">
+      <label for="email">Email address</label>
+      <input type="email" class="form-control" id="email" formControlName="email">
+    </div>
+    <div class="form-group">
+      <label for="firstName">First Name</label>
+      <input type="text" class="form-control" id="firstName" formControlName="firstName">  
+    </div>
+    <div class="form-group">
+      <label for="lastName">Last Name</label>
+      <input type="text" class="form-control" id="lastName" formControlName="lastName">
+    </div>
+  </div>
+  
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" formControlName="password">
+  </div>
+    
+  <div formGroupName="customer">
+  
+  
+    <div formGroupName="address">
+    
+      <div class="form-group">
+        <label for="street">Street</label>
+        <input type="text" class="form-control" id="street" formControlName="street">
+      </div>
+      <div class="form-group">
+        <label for="city">City</label>
+        <input type="text" class="form-control" id="city" formControlName="city">
+      </div>
+      <div class="form-group">
+        <label for="postalCode">Postal Code</label>
+        <input type="text" class="form-control" id="postalCode" formControlName="postalCode">
+      </div>
+      <div class="form-group">
+        <label for="country">Country</label>
+        <input type="text" class="form-control" id="country" formControlName="country">
+      </div>
 
+    </div>
+    
+    <div formGroupName="creditCard">
+    
+      <div class="form-group">
+        <label for="type">Credit Card Type</label>
+        <input type="text" class="form-control" id="type" formControlName="type">
+      </div>    
+      <div class="form-group">
+        <label for="number">Credit Card Number</label>
+        <input type="text" class="form-control" id="number" formControlName="number">
+      </div>    
+      <div class="form-group">
+        <label for="expirationMonth">Credit Card Expiration Month</label>
+        <input type="number" class="form-control" id="expirationMonth" formControlName="expirationMonth">
+      </div>    
+      <div class="form-group">
+        <label for="expirationYear">Credit Card Expiration Year</label>
+        <input type="number" class="form-control" id="expirationYear" formControlName="expirationYear">
+      </div>
 
-
-<label>
-  <span>Email:</span>
-  <input type="text" formControlName="email">
-</label>
-First Name: <input type="text" formControlName="firstName">
-Last Name: <input type="text" formControlName="lastName">
-
-
-<div formGroupName="address">
-
-Street: <input type="text" formControlName="street">
-City: <input type="text" formControlName="city">
-Postal Code: <input type="text" formControlName="postalCode">
-Country: <input type="text" formControlName="country">
-
-</div>
-
-<div formGroupName="creditCard">
-
-Type: <input type="text" formControlName="type">
-Number: <input type="text" formControlName="number">
-Expiration Month: <input type="number" formControlName="expirationMonth">
-Country: <input type="number" formControlName="expirationYear">
-
-</div>
-
-</div>
-
-Password: <input type="password" formControlName="password">
-
-<button type="submit" class="btn btn-default">Submit</button>
+    </div>
+  
+  </div>
+  
+  <button type="submit" class="btn btn-default">Submit</button>
 
 </form>
 `
